6ca5ce998c40033ee821db0543af16088ef8cebe
ABID Aziz <83600576+Aziz19979@users.noreply.github.com>
2022-12-01 11:34:13 +0100

    Create pixels

---
+++ src/main/java/org/ecn/PgmUtils.java
@@ -0,0 +1,18 @@
+package org.ecn;
+import lombok.Data;
+import java.util.HashMap;
+
+public class PgmUtils {
+
+    public static int[] pgmUtile(PgmDataImage pgmDataImage) {
+        int[] pixels = new int[1000];
+        for (int i = 0; i < pgmDataImage.getHeight(); i++) {
+            for (int j = 0; j < pgmDataImage.getWidth(); j++) {
+                pixels[ pgmDataImage.data[i][j]] += 1;
+            }
+        }
+
+        return pixels;
+    }
+
+}
