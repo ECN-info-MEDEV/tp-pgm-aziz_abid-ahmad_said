ccdf873c460f52667460c44060a1538ec9d0fc29
Ahmad Said <47715383+Ahmad-Said@users.noreply.github.com>
2022-12-01 08:45:03 +0100

    add images

---
+++ src/main/java/org/ecn/ResourcesHelper.java
@@ -0,0 +1,23 @@
+package org.ecn;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class ResourcesHelper {
+	private static final ResourcesHelper RESOURCE_INSTANCE = new ResourcesHelper();
+	private static final String PACKAGE_SCOOP = "/org/ecn";
+
+	/** Parameter in the form /fxml/Some.fxml or /img/Some.png */
+	public static URL getResourceAsURL(String resourcesName) {
+		return RESOURCE_INSTANCE.getClass().getResource(PACKAGE_SCOOP + resourcesName);
+	}
+
+	/** Parameter in the form /fxml/Some.fxml or /img/Some.png */
+	public static InputStream getResourceAsStream(String resourcesName) {
+		return RESOURCE_INSTANCE.getClass().getResourceAsStream(PACKAGE_SCOOP + resourcesName);
+	}
+
+	public static String getResourceAsString(String resourcesName) {
+		return PACKAGE_SCOOP + resourcesName;
+	}
+}
