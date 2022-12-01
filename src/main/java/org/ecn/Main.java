057b2b39128d0bbdd37f9f4d00859854681876f5
Ahmad Said <47715383+Ahmad-Said@users.noreply.github.com>
2022-12-01 09:55:43 +0100

    Update Main.java

--- src/main/java/org/ecn/Main.java
+++ src/main/java/org/ecn/Main.java
@@ -1,7 +1,26 @@
 package org.ecn;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String fichierPgm = "/img/brain.pgm";
+        PgmDataImage pgmDataImage = null;
+        try {
+            File ficherPath = new File(ResourcesHelper.getResourceAsURL(fichierPgm).toURI());
+            pgmDataImage = PgmReader.readImage(ficherPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Failed to read image !");
+            System.exit(1);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        DisplayImageInAwt displayImageInAwt = new DisplayImageInAwt(fichierPgm, pgmDataImage);
     }
 }
